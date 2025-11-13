ee322d38f1e4cea28a05809b1d3f56a1d97c1610
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2025-11-13 22:37:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import express from 'express';
-
+import express from "express";  
+import students from './routes/student.js';
+import teacher from './routes/teachers.js';
 const app = express();
 
-// Router params
-app.get('/student/delete/:id', (_req,res) => {
-  res.send(_req.params.id);
-})
-app.listen(8000, () => console.log("Server Up!"));
+
+
+app.use('/Teachers', teacher);
+app.use('/students', students);
+
+app.listen(8000, ()=> console.log("Server Up!"));
