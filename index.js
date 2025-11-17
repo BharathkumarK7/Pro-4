5d5e98ffe6751cb1a78dd6e7bcd7f84afaf6c41e
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2025-11-17 22:45:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import express from 'express';
 const app = express();
 
 // Router params
-app.get('/student/delete/:id', (_req,res) => {
-  res.send(_req.params.id);
+// app.get('/student/delete/:id', (_req,res) => {
+  app.get('/ecom/products/iphone/:model', (_req,res) => {
+    const{model} = _req.params;
+  // res.send("iphone",_req.params.model);
+  res.send(`iphone${model} pro max` )
 })
 app.listen(8000, () => console.log("Server Up!"));
+  
