bef2601da39f2c621ddb3b0969f73389fc251683
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2025-11-18 21:28:58 +0530

    Add files via upload

--- index2.js
+++ index2.js
@@ -1,21 +1,11 @@
-import express from 'express';
+import express from "express";
+
 const app = express();
 
 
-const cb1 = (_req, _res, next)=> {
-  console.log('First Callback')
-  next()
-}
+app.get("/product/order/:day/:month/:year",  (_req,_res) => {
+  const {day, month, year} = _req.params;
+  _res.send(`product was ordered on: ${day}/${month}/${year}`)
+})
 
-const cb2 = (_req,_res, next) => {
-  console.log('Second Callback')
-  next()
-}
-const cb3 = (_req, _res) => {
-  console.log('Third Callback');
-  _res.send("Array of Callbacks"); //// VERY IMPORTANT    THIS UNDERSCORE _res _res _req _req
-}
-
-app.get("/array-cb",[cb1, cb2, cb3] );
-
-app.listen(8000, () => console.log("Server Up!")) ;
+app.listen(8000, () => console.log("Server UP!"));
