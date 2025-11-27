d4d2e0548e8bec58fdc8a27368570a5089eec294
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2025-11-27 23:05:00 +0530

    Add files via upload

--- student.js
+++ student.js
@@ -1,20 +1,19 @@
 import express from "express";
-const router = express.Router();
 
-router.get("/all", (_req,res) => {
+const allstudents = (_req,res) => {
   res.send("All Students")
-}); 
+}; 
 
-router.post("/create", (_req,res) => {
+const newstudent  = (_req,res) => {
   res.send("Sign in New Student");
-});
+};
 
-router.put("/update", (_req,res) => {
+const updatestudent = (_req,res) => {
   res.send("Update Student Id");
-});
+};
 
-router.delete("/delete", (_req,res) => {
+const deletestudent = (_req,res) => {
   res.send("Remove student From the DB.");
-});
+};
 
-export default router;
+export { allstudents, newstudent, updatestudent, deletestudent };
