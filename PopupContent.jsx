a9b350ef8dcb409b33ac65d35d9b98a22d5d23bc
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2025-12-15 21:37:25 +0530

    Add files via upload

---
+++ PopupContent.jsx
@@ -0,0 +1,15 @@
+const PopupContent = ({ copied }) => {
+  return (
+    <section> 
+      {copied && (
+        <div style = {{ position: 'absolute', bottom: '3rem'}}>
+          Copied to Clipboard</div>
+
+
+
+      )}
+    </section>
+  )
+}
+
+export default PopupContent 
