b58621543c361057a5190fd161e4514caaf73e6b
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2025-12-15 21:38:05 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Counter from './comps/Counter'
 import Profile from './comps/Profile.jsx'
 import Shoppinglist from './comps/Shoppinglist.jsx'
-
+import CopyInput from './comps/CopyInput.jsx' 
+import PopupContent from './comps/PopupContent.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,6 +13,7 @@ function App() {
       <Counter />
       <Profile />
       <Shoppinglist />
+      <CopyInput />
         </>
   )
 }
