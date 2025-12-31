c31851e46f20421ea20dda75d370a53b1a3be5e4
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2025-12-31 22:47:20 +0530

    Add files via upload

--- CompA.jsx
+++ CompA.jsx
@@ -1,9 +1,18 @@
+// CompA.jsx
+import React from "react";
+import CompB from "./CompB";
+import CompC from "./CompC";
+import CompD from "./CompD";
+import CompE from "./CompE";
 const CompA = () => {
   return (
     <div>
-    <h1>Hello</h1>
+      <CompB />
+      <CompC />
+      <CompD />
+      <CompE/>
     </div>
-  )
-}
+  );
+};
 
 export default CompA;
