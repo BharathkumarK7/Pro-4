b68e9dfa3e510cbace55c0d1199d55841b4e0bab
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2025-12-31 22:48:29 +0530

    Add files via upload

--- CompB.jsx
+++ CompB.jsx
@@ -1,9 +1,14 @@
-
+// CompB.jsx
+import React, { useContext } from "react";
+import { DataContext } from "../App";
 const CompB = () => {
+  const name = useContext(DataContext);
+
   return (
     <div>
-    CompC /</div>
-  )
-}
+      <p>Name from Context: {name}</p>
+    </div>
+  );
+};
 
 export default CompB;
