595caaf7fee36b37f23fcb944b8f009b8d6c44b9
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2026-01-06 21:40:13 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Footer
+      <footer>
+        <p> 2025 My Website. All rights reserved.</p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
