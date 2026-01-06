5a574f3213308a64841905d0f2ec499e475c0fca
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2026-01-06 21:40:35 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Header
+      <h1>Welcome to My website</h1>
+      <nav>
+        <a href="https://github.com/BharathkumarK7">HOME</a>
+        <a href="">ABOUT</a>
+        <a href="">CONTACT</a>
+      </nav>
+    </div>
+  )
+}
+
+export default Header;
