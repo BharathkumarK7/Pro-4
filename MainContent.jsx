2478e789fafea0327a1b92cf8a0aa9644045deef
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2026-01-06 21:41:04 +0530

    Add files via upload

---
+++ MainContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <div>MainContent
+      <main>
+        <h2>Main Content</h2>
+        <p className='paraclass'>Element with the text of your choice.</p>
+      </main>
+    </div>
+  )
+}
+
+export default MainContent;
