10b0814d19423365d95493bd1c158488843a47ea
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2026-01-06 21:42:40 +0530

    Add files via upload

---
+++ WelcomeMessage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+const WelcomeMessage = () => {
+
+  const myname = "Bharath";
+  return (
+    <div>WelcomeMessage
+      <h1>"Hello World!" </h1>
+      <p>Welcome to learning the jsx!</p>
+      <h3>My name: {myname}</h3>
+    </div>
+  )
+}
+
+export default WelcomeMessage;
