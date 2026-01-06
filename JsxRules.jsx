7ab4d15d71bb6395b0810c3c6792535a17d1d23f
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2026-01-06 21:40:54 +0530

    Add files via upload

---
+++ JsxRules.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const JsxRules = () => {
+  return (
+    <div><h1>JSX Rules</h1>
+      <p className='paraclass'>
+        1. JSX must return a SINGLE PARENT ELEMENT. <br />
+        2. JSX must be PROPERLY CLOSED. <br />
+        3. JSX attributes are written using CAMELCASE. <br />
+        
+      </p>
+    </div>
+  )
+}
+
+export default JsxRules
