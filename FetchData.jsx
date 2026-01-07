d5a264ef8b41225481390e8dd0fb03cca63abc3c
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2026-01-07 22:11:51 +0530

    Add files via upload

---
+++ FetchData.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+
+
+const FetchData = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect (() => {
+   const fetchData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const data = await response.json()
+    setPosts(data)
+    
+   }
+     fetchData();
+
+  }, [])
+
+
+
+  return (
+    <div>FetchData
+      <h1>First Post Title:</h1>
+        {posts.length > 0 ?  <h2> {posts[0].title} </h2>: <p>Loading....</p>}
+      
+    </div>
+  )
+};    
+
+export default FetchData;
