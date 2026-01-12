d43a3594a0f1c553ead89fa039d37a307d6636e5
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2026-01-12 22:33:58 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
-import Newuser from './Comps/Newuser';
 
 const App = () => {
   return (
     <div>
       App
-      <Newuser />
+      <section>
+        <h1>Let's Make a Webpage for the Arena. </h1>
+        <h2>
+          It provides Information about the Arena. The platform for people to
+          gather online together for thrilling fun.
+        </h2>
+        <h2>
+          This creates a space for people to come together and have fun without
+          any limitaions.
+        </h2>
+
+        <div>
+          <p>Here is the information about the Arena:</p>
+          <ul>
+            <li>
+              It is a platform for people to gather online together for
+              thrilling fun.
+            </li>
+            <li>
+              This creates a space for people to come together and have fun
+              without any limitations.
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h1>Rules</h1>
+          <h5>
+            All members are requested to respect each other to promote peaceful
+            environment.
+          </h5>
+          <h5>
+            Any form of harassment or discrimination will not be tolerated.
+          </h5>
+          <h5>
+            Follow the guidelines provided by the moderators to ensure a safe
+            and enjoyable experience for everyone.
+          </h5>
+          <h3>Behave yourself to save yourself a spot in here.</h3>
+        </div>
+      </section>
     </div>
   );
 };
