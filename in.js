e54684624e42828844bd7f8a7dc876dba736a38a
BharathkumarK7 <121218360+BharathkumarK7@users.noreply.github.com>
2026-01-16 22:27:31 +0530

    Add files via upload

--- in.js
+++ in.js
@@ -5,4 +5,28 @@ app.get ('/', (_req, res) => {
   res.send("Home Sweet Home!");
 })
 
+
+app.put ('/', (_req, res) => {
+  res.send("Vanakam da !")
+})
+
+app.post('/', (_req, res) => {
+  res.send("Hello Nice to meet you!")
+})
+
+app.patch ('/', (_req,res) => {
+  res.send("Patcher!")
+})
+
+app.delete('/', (_req,res) => {
+  res.send("Delete the garbage values in the memory!")
+})
+
+app.head ('/', (_req,res) => {
+  res.send("The head portion is always correct Listen to it!")
+})
+
+app.options('/', (_req,res) => {
+  res.send("The result must always have options")
+})
 app.listen(8500,() => console.log("Server UP!"));
